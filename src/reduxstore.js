85ec9b475d53062bdca0aea3f404c4c22430d0af
Lily Zeiset <lilyzeiset@gmail.com>
2023-05-07 16:44:57 -0400

    removed unused apis from reduxstore and removed unused files

--- src/reduxstore.js
+++ src/reduxstore.js
@@ -1,22 +1,16 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {itemApi} from './api/itemApi';
-import {categoryApi} from './api/categoryApi';
 import {warehouseApi} from './api/warehouseApi';
-import {lotApi} from './api/lotApi';
 
 //bunch of useReducers put together
 const store = configureStore({
     reducer: {
         [itemApi.reducerPath]: itemApi.reducer,
-        [categoryApi.reducerPath]: categoryApi.reducer,
         [warehouseApi.reducerPath]: warehouseApi.reducer,
-        [lotApi.reducerPath]: lotApi.reducer
     },
     middleware: (defaultMiddleware) => defaultMiddleware()
                                         .concat(itemApi.middleware)
-                                        .concat(categoryApi.middleware)
                                         .concat(warehouseApi.middleware)
-                                        .concat(lotApi.middleware)
 });
 
 export default store;
